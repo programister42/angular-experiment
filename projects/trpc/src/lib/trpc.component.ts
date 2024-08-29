afbcacee129b83a18df09e99fc222f3578c3bb1d
Taras Tereschenko <tarrrasio@gmail.com>
2024-08-29 22:19:53 +0500

    init: generate trpc lib

---
+++ projects/trpc/src/lib/trpc.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-trpc',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      trpc works!
+    </p>
+  `,
+  styles: ``
+})
+export class TrpcComponent {
+
+}
---
+++ projects/trpc/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of trpc
+ */
+
+export * from './lib/trpc.service';
+export * from './lib/trpc.component';
